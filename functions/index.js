0a20919ac9a7c6a512302d95071c747bc596d598
lyanita <anitaly.ubc@gmail.com>
2021-12-30 22:05:51 -0800

    Added GraphQL playground

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 const functions = require("firebase-functions");
 const express = require("express");
 const {ApolloServer} = require("apollo-server-express");
+const {ApolloServerPluginLandingPageGraphQLPlayground} = require("apollo-server-core");
 
 const {typeDefs, resolvers} = require("./graphql/schema");
 
@@ -9,17 +10,12 @@ const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  plugins: [
+    // eslint-disable-next-line new-cap
+    ApolloServerPluginLandingPageGraphQLPlayground(),
+  ],
 });
 
 server.applyMiddleware({app, path: "/", cors: true});
 
 exports.graphql = functions.https.onRequest(app);
-
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
