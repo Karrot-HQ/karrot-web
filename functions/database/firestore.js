8e84445df96d590757bbc2c58c2e97c05c645a21
lyanita <anitaly.ubc@gmail.com>
2022-01-01 00:52:01 -0800

    Test github actions

--- functions/database/firestore.js
+++ functions/database/firestore.js
@@ -1,12 +1,12 @@
 const admin = require("firebase-admin");
-const path = require("path");
-require("dotenv").config({path: path.join(__dirname, "../.env")});
+// const path = require("path");
+// require("dotenv").config({path: path.join(__dirname, "../.env")});
 
 // Update the credentials path
-const serviceAccount = require(process.env.FIREBASE_SERVICE_ACCOUNT_PATH);
+// const serviceAccount = require(process.env.FIREBASE_SERVICE_ACCOUNT_PATH);
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  // credential: admin.credential.cert(serviceAccount),
 });
 
 const db = admin.firestore();
