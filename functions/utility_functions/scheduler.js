1f9cf8ef35cc93ad895fdda52fb5519868a19c62
lyanita <anitaly.ubc@gmail.com>
2022-01-09 14:01:05 -0800

    Fixed scheduler

--- functions/utility_functions/scheduler.js
+++ functions/utility_functions/scheduler.js
@@ -8,7 +8,7 @@ rule.hour = 0; // runs once a day at midnight
 const expiryCheck = async () => {
   const currentDate = new Date();
   const expiredItems = await db.collection("inventories").where("expiry_tag", "==", false)
-      .where("expiry_date", ">", currentDate).get();
+      .where("expiry_date", "<", currentDate).get();
   expiredItems.forEach((doc) => {
     const userId = doc.user_id;
     const itemId = doc.item_id;
@@ -18,7 +18,12 @@ const expiryCheck = async () => {
 };
 
 // eslint-disable-next-line no-unused-vars
-const expiryCheckScheduler = schedule.scheduleJob(rule, () => {
+/* const expiryCheckScheduler = schedule.scheduleJob(rule, () => {
+  expiryCheck();
+  console.log("Finished expiry check.");
+});*/
+
+const expiryCheckScheduler = schedule.scheduleJob("*/5* * * *", () => {
   expiryCheck();
   console.log("Finished expiry check.");
 });
